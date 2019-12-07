6916367640a41c5857921a8272e3b31bc4857fc7
baredh821 <baredh821@gmail.com>
2019-12-06 21:14:42 -0600

    Files Updated

--- Develop/server.js
+++ Develop/server.js
@@ -3,12 +3,12 @@ const htmlRoutes = require("./routes/htmlRoute");
 const apiRoutes = require("./routes/apiRoute");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-//app.use("/api", apiRoutes);
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 app.listen(PORT,()=> console.log(`Listening on PORT : ${PORT}`));
